4e5700fe78167a97f63c11e4119113834ad48a70
Jesse Luehrs <doy at tozt dot net>
2008-06-04 02:48:04 +0000

    move the map_manager stuff into drawmodesettings

--- DrawModeSettings.ml
+++ DrawModeSettings.ml
@@ -4,3 +4,14 @@ type tool = ArrowTool | LineTool |
             TextTool  | ObjTool
 
 let active_tool = ref ArrowTool
+let grid_size = ref 3
+let display_grid = ref true
+let constrain_to_grid = ref false
+let show_monsters = ref true
+let show_objects = ref true
+let show_scenery = ref true
+let show_players = ref true
+let show_goals = ref true
+let show_sounds = ref true
+let show_annotations = ref true
+let vm_crosshair = ref true
