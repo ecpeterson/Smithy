35be0bd22f0435f9e47881851e3b1ead958ab0d9
Jesse Luehrs <doy at tozt dot net>
2008-10-09 03:13:31 +0000

    save the main window width and height in the preferences

--- Smithy.ml
+++ Smithy.ml
@@ -6,23 +6,24 @@ let okay_to_quit = ref true
 
 (* honest to god entry point for the program. *)
 let _ =
+    let scale, width, height = Preferences.load_prefs () in
     ignore (GtkMain.Main.init ());
     Gdk.Rgb.init ();
     GtkBase.Widget.set_default_visual (Gdk.Rgb.get_visual ());
     GtkBase.Widget.set_default_colormap (Gdk.Rgb.get_cmap ());
-    let main_window = new DrawModeWindows.drawModeWindow ~width:500 ~height:300
+    let main_window = new DrawModeWindows.drawModeWindow ~width ~height
                                                          ~title:"Smithy"
                                                          ~show:true () in
     DrawMode.init_draw_mode main_window;
     let at_exit _ =
         if !okay_to_quit then begin
             okay_to_quit := false;
-            Preferences.save_prefs main_window#orthodrawer#scale;
+            Preferences.save_prefs main_window#orthodrawer#scale
+                                   main_window#width main_window#height;
             GMain.Main.quit ()
         end in
     ignore (main_window#window#event#connect#delete ~callback:(fun _ -> false));
     ignore (main_window#window#connect#destroy ~callback:at_exit);
-    let scale = Preferences.load_prefs () in
     main_window#orthodrawer#set_scale scale;
     let args = Sys.argv in
     if Array.length args > 1 then
